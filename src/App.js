095cd5134cf11cb1cfb369e4f04d703b0077bf87
ecobiziimshillong <85777997+ecobiziimshillong@users.noreply.github.com>
2021-06-12 20:25:59 +0530

    deploy my react app to github pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Welcome to EcoBiz React App </h1>
+	  <h2> Current Author: EcoBiz Batch PGP20 </h2>
+    </div>
+  );
+}
+
+export default App;
